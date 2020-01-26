53a912c6d8f52b48432cfe49df603cbc88cf2d3e
Sharon <sharonjoji99@gmail.com>
2020-01-26 13:31:59 +0530

    final , using stateless and resuable dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,46 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
-void main() => runApp(MyApp());
+
+void main() => runApp(MaterialApp(
+  home:  QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes= [
+    Quote(author: 'auth1', text:'quote1'),
+    Quote(author: 'auth2', text:'quote2'),
+    Quote(author: 'auth3', text:'quote3'),
+  ];
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text(
+          "Awesome Quotes",
+
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quotes){
+          return QuoteCard(quote:quotes);
+      }).toList()
+
+      ),
+    );
+  }
+}
+
 
---
+++ lib/quote.dart
@@ -0,0 +1,11 @@
+class Quote {
+  String text;
+  String author;
+  Quote({this.text,this.author}) {
+  //Quote({String text,String author}) {
+//    this.text = text;
+//    this.author = author;
+  }
+}
+
+Quote myQuote=Quote(text:"test qupte",author:"sharon");
